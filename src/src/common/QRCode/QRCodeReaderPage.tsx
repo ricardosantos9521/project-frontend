211f70fdd2907a1f2be8fdf1bc78bd21d2eb6ee0
ricardo <ricardo.santos9521@outlook.com>
2019-05-23 20:37:28 +0100

    change vibrate to a unique value

--- src/src/common/QRCode/QRCodeReaderPage.tsx
+++ src/src/common/QRCode/QRCodeReaderPage.tsx
@@ -23,7 +23,7 @@ class QrCodeReaderPage extends React.Component<IProps, IState> {
     }
 
     onScan(data: string) {
-        window.navigator.vibrate([200, 200, 200, 200]);
+        window.navigator.vibrate(200);
         this.setState({ qrCodeString: data });
     }
 
