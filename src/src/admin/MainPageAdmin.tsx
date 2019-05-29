d9809f3178b762cfe404a20d6028c51004e745a7
ricardo <ricardo.santos9521@outlook.com>
2019-05-29 11:56:55 +0100

    add navigationpage to mainpageadmin

--- src/src/admin/MainPageAdmin.tsx
+++ src/src/admin/MainPageAdmin.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, RouteComponentProps } from "react-router-dom";
 import { StaticContext } from "react-router";
 import QrCodeReaderPage from "../common/QRCode/QRCodeReaderPage";
+import NavigationPage, { MenuItem } from "../common/UIPages/NavigationPage";
+import Settings from "../common/Settings";
 
 interface IProps {
     pathParent: string
@@ -10,19 +12,35 @@ interface IProps {
 class MainPageAdmin extends React.Component<IProps>{
 
     render() {
+        let items: Array<MenuItem> = [
+            {
+                icon: "qrcode",
+                name: "QRCode",
+                onClick: () => {
+                    Settings.history.push({ pathname: '/admin/qrreader' });
+                }
+            }
+        ];
+
+        let overflowItems: Array<MenuItem> = [
+
+        ];
+
         return (
             <div className="admin-container" style={{ width: "100%", height: "100%" }}>
-                <Route
-                    path={this.props.pathParent + "/qrreader"}
-                    render={
-                        (props: RouteComponentProps<any, StaticContext, any>) => {
-                            return (
-                                <QrCodeReaderPage />
-                            )
+                <NavigationPage items={items} overflowItems={overflowItems}>
+                    <Route
+                        path={this.props.pathParent + "/qrreader"}
+                        render={
+                            (props: RouteComponentProps<any, StaticContext, any>) => {
+                                return (
+                                    <QrCodeReaderPage />
+                                )
+                            }
                         }
-                    }
-                    key="qrreader"
-                />
+                        key="qrreader"
+                    />
+                </NavigationPage>
             </div>
         );
     }
