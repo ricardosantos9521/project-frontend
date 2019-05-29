15be2eddb256c407c829436d1a2f14eb8f4f8c3c
ricardo <ricardo.santos9521@outlook.com>
2019-05-29 11:59:20 +0100

    add navigationpage to mainpageuser

--- src/src/user/MainPageUser.tsx
+++ src/src/user/MainPageUser.tsx
@@ -3,6 +3,8 @@ import { Route, RouteComponentProps } from "react-router-dom";
 import { StaticContext } from "react-router";
 import QrCodePage from "../common/QRCode/QRCodePage";
 import IProfile from "../common/Profile/IProfile";
+import NavigationPage, { MenuItem } from "../common/UIPages/NavigationPage";
+import Settings from "../common/Settings";
 
 interface IProps {
     pathParent: string,
@@ -12,19 +14,35 @@ interface IProps {
 class MainPageUser extends React.Component<IProps>{
 
     render() {
+        let items: Array<MenuItem> = [
+            {
+                icon: "qrcode",
+                name: "QRCode",
+                onClick: () => {
+                    Settings.history.push({ pathname: '/user/qrcode' });
+                }
+            }
+        ];
+
+        let overflowItems: Array<MenuItem> = [
+
+        ];
+
         return (
             <div className="user-container" style={{ width: "100%", height: "100%" }}>
-                <Route
-                    path={this.props.pathParent + "/qrcode"}
-                    render={
-                        (props: RouteComponentProps<any, StaticContext, any>) => {
-                            return (
-                                <QrCodePage value={this.props.profile.firstName} />
-                            )
+                <NavigationPage items={items} overflowItems={overflowItems}>
+                    <Route
+                        path={this.props.pathParent + "/qrcode"}
+                        render={
+                            (props: RouteComponentProps<any, StaticContext, any>) => {
+                                return (
+                                    <QrCodePage value={this.props.profile.firstName} />
+                                )
+                            }
                         }
-                    }
-                    key="qrcode"
-                />
+                        key="qrcode"
+                    />
+                </NavigationPage>
             </div>
         );
     }
