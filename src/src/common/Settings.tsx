c9f0aa50ccdcf51271506608d82d93e57d8056f5
Ricardo <ricardo.santos9521@outlook.com>
2019-06-13 17:17:44 +0100

    update fluent to version 7

--- src/src/common/Settings.tsx
+++ src/src/common/Settings.tsx
@@ -1,7 +1,8 @@
 import { History, createBrowserHistory } from "history";
 
 class Settings {
-    public static serverUrl: string = "https://rics.synology.me/backendproject/";
+    // public static serverUrl: string = "https://rics.synology.me/backendproject/";
+    public static serverUrl: string = "http:/172.17.215.184:5000";
 
     public static history: History<any> = createBrowserHistory({
         basename: '/reactproject'
