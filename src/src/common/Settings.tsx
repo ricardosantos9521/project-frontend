c1d2f15d38c25bbc389eadaf11ffe48741fe7cb7
Ricardo <ricardo.santos9521@outlook.com>
2019-06-13 17:28:07 +0100

    serverUrl wrong

--- src/src/common/Settings.tsx
+++ src/src/common/Settings.tsx
@@ -1,8 +1,8 @@
 import { History, createBrowserHistory } from "history";
 
 class Settings {
-    // public static serverUrl: string = "https://rics.synology.me/backendproject/";
-    public static serverUrl: string = "http:/172.17.215.184:5000";
+    public static serverUrl: string = "https://rics.synology.me/backendproject/";
+    // public static serverUrl: string = "http://localhost:5000";
 
     public static history: History<any> = createBrowserHistory({
         basename: '/reactproject'
