821b5ac2f30d5f97c9aa6dd3ad0efbac2e063d73
Ricardo <ricardo.santos9521@outlook.com>
2019-07-15 17:05:45 +0100

    return boolean in setAuthorizationHeader if accessToken is not null

--- src/src/common/Helpers/Authorization.tsx
+++ src/src/common/Helpers/Authorization.tsx
@@ -1,8 +1,10 @@
 import Auth from "../Backend/Auth";
 
-export async function setAuthorizationHeader(request: XMLHttpRequest) {
+export async function setAuthorizationHeader(request: XMLHttpRequest): Promise<Boolean> {
     var accessToken = await Auth.GetAccessToken();
-    if (accessToken != null) {
+    if (accessToken !== null) {
         request.setRequestHeader("Authorization", "Bearer " + accessToken!.token);
+        return true;
     }
+    return false;
 }
