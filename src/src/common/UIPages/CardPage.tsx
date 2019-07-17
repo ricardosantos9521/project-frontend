dd88a98f93430687bb3e0be044bde34a7a578ef5
Ricardo <ricardo.santos9521@outlook.com>
2019-07-17 23:09:33 +0100

    update cardpage ui

--- src/src/common/UIPages/CardPage.tsx
+++ src/src/common/UIPages/CardPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import './CardPage.css'
 import { Depths } from "@uifabric/fluent-theme/lib/fluent/FluentDepths";
 import { Spinner, SpinnerSize } from "office-ui-fabric-react/lib/Spinner";
+import { Stack } from "office-ui-fabric-react/lib/Stack";
 
 interface IProps {
     isLoading?: boolean,
@@ -12,29 +12,19 @@ interface IProps {
 class CardPage extends React.Component<IProps> {
     render() {
         return (
-            <div className="cardpage">
+            <Stack className="files" tokens={{ childrenGap: 20 }} horizontal disableShrink wrap horizontalAlign="center" style={{ height: "100%" }} verticalAlign={(this.props.isLoading!) ? "start" : "center"}>
                 {
-                    (!this.props.isLoading || this.props.isLoading === undefined) ?
+                    (this.props.isLoading!) ?
                         (
-                            <div className="cardcenter" style={{ boxShadow: Depths.depth64, width: this.props.widthCard }}>
-                                <div className="cardContent">
-                                    {this.props.children}
-                                </div>
-                            </div>
+                            <Spinner size={SpinnerSize.large} />
                         ) :
                         (
-                            <div className="cardcenter">
-                                <Spinner size={SpinnerSize.large} />
-                                {
-                                    (this.props.loadingMessage !== undefined) &&
-                                    (
-                                        <h4>{this.props.loadingMessage}</h4>
-                                    )
-                                }
+                            <div style={{ boxShadow: Depths.depth64, width: this.props.widthCard, position: "absolute" }}>
+                                {this.props.children}
                             </div>
                         )
                 }
-            </div>
+            </Stack>
         )
     }
 }
