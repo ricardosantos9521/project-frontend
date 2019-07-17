1819bb797a41ff9858678b3550a083f02a88ef1f
Ricardo <ricardo.santos9521@outlook.com>
2019-07-17 22:29:43 +0100

    add optional props to children in customRoute

--- src/src/common/Helpers/CustomRoute.tsx
+++ src/src/common/Helpers/CustomRoute.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import { Route, RouteComponentProps } from "react-router-dom";
 import { StaticContext } from "react-router";
 import { Label } from "office-ui-fabric-react/lib/Label";
@@ -17,7 +17,9 @@ export class CustomRoute extends React.Component<IProps>{
                 render={
                     (props: RouteComponentProps<any, StaticContext, any>) => {
                         if (this.props.showRender === undefined || this.props.showRender!)
-                            return this.props.children
+                            return React.Children.map(this.props.children, child => {
+                                return React.cloneElement(child as ReactElement<any>, { routeProps: props });
+                            });
                         else
                             return (
                                 <Stack horizontalAlign="center" verticalAlign="center" style={{ height: "100%" }}>
