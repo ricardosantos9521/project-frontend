b845474f6e38e070aa3054ff8d058b76ca94e0bc
Ricardo <ricardo.santos9521@outlook.com>
2019-07-18 11:50:01 +0100

    remove unecessary code from CardPage and update profilepage with changes

--- src/src/common/UIPages/CardPage.tsx
+++ src/src/common/UIPages/CardPage.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Depths } from "@uifabric/fluent-theme/lib/fluent/FluentDepths";
 import { Stack } from "office-ui-fabric-react/lib/Stack";
-import './CardPage.css'
 import SpinnerComponent from "./SpinnerComponent";
 
 interface IProps {
     isLoading?: boolean,
-    loadingMessage?: string,
-    widthCard?: string | number | undefined
+    loadingMessage?: string
 }
 
 class CardPage extends React.Component<IProps> {
@@ -17,7 +15,7 @@ class CardPage extends React.Component<IProps> {
 
         return (
             <Stack tokens={{ childrenGap: 20 }} verticalAlign="center" horizontalAlign="center" styles={{ root: { height: "100%" } }}>
-                <div className="card" style={{ boxShadow: Depths.depth64, width: this.props.widthCard, position: "relative" }}>
+                <div className="card" style={{ boxShadow: Depths.depth64, position: "relative" }}>
                     {this.props.children}
                 </div>
             </Stack>
